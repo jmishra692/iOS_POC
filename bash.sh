3b5df7e2e9a3e0f0a300a28735a4f62bc5f21a0a
jmishra692 <jmishra.c@arlo.com>
2024-11-11 18:45:26 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://jmishra.c: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@spruce.arlo.com/apps/ios.git
+git clone https://jmishra.c:$PAT@spruce.arlo.com/apps/ios.git
 
 chmod +x ios.sh
 ./ios.sh
