3b75cea2d59772135cb70acd114c006200983de6
jmishra692 <jmishra.c@arlo.com>
2024-11-12 22:48:29 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Ensure the script exits on any error
-set -e
-
-# Check if the PAT variable is set
-if [ -z "$PAT" ]; then
-  echo "Error: PAT environment variable is not set."
+# Check if PAT is set
+if [ -z "$BUILDKITE_SECRETS_PAT" ]; then
+  echo "Error: BUILDKITE_SECRETS_PAT is not set."
   exit 1
 fi
 
-# Clone the repository using the PAT
-git clone "https://jmishra.c:${PAT}@spruce.arlo.com/apps/ios.git"
+# Clone the repository
+git clone "https://jmishra.c:${BUILDKITE_SECRETS_PAT}@spruce.arlo.com/apps/ios.git"
