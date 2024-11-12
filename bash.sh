55a5a11d379a85ff1bdcc57c2d1ef59c5432192c
jmishra692 <jmishra.c@arlo.com>
2024-11-12 22:34:29 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git clone https://jmishra.c:$PAT@spruce.arlo.com/apps/ios.git
+# Ensure the script exits on any error
+set -e
 
-chmod +x ios.sh
-./ios.sh
+# Check if the PAT variable is set
+if [ -z "$PAT" ]; then
+  echo "Error: PAT environment variable is not set."
+  exit 1
+fi
+
+# Clone the repository using the PAT
+git clone "https://jmishra.c:${PAT}@spruce.arlo.com/apps/ios.git"
